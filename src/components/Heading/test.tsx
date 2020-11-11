29e7bdeafda37499a4f89a791b05ee9e331cfde2
Jadir Junior <jadirjsjunior@gmail.com>
2020-11-11 15:48:44 -0300

    test a black heading

---
+++ src/components/Heading/test.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Heading from '.'
+
+describe('<Heading />', () => {
+  it('should render a white heading by default', () => {
+    renderWithTheme(<Heading>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black heading when passed', () => {
+    renderWithTheme(<Heading color="black">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      color: '#030517'
+    })
+  })
+})
