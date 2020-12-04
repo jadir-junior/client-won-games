c63512764ec12b2c8372608471b58486f0ed7421
Jadir Junior <jadirjsjunior@gmail.com>
2020-12-04 14:30:18 -0300

    create profile page

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,10 @@
+import ProfileForm from 'components/ProfileForm'
+import Profile from 'templates/Profile'
+
+export default function Me() {
+  return (
+    <Profile>
+      <ProfileForm />
+    </Profile>
+  )
+}
