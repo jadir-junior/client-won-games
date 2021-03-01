234f2406e98caf3d508ce9807a678426a648a324
Jadir Junior <jadirjsjunior@gmail.com>
2021-03-01 11:37:51 -0300

    should render dropdown content with cart items and total

---
+++ src/components/CartDropdown/index.tsx
@@ -0,0 +1,21 @@
+import * as S from './styles'
+
+import CartIcon from 'components/CartIcon'
+import CartList from 'components/CartList'
+import Dropdown from 'components/Dropdown'
+import { GameItemProps } from 'components/GameItem'
+
+export type CartDropdownProps = {
+  items: GameItemProps[]
+  total: string
+}
+
+const CartDropdown = ({ items, total }: CartDropdownProps) => (
+  <S.Wrapper>
+    <Dropdown title={<CartIcon quantity={items.length} />}>
+      <CartList items={items} total={total} hasButton />
+    </Dropdown>
+  </S.Wrapper>
+)
+
+export default CartDropdown
---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,8 @@
+import { Wrapper as CartWrapperStyles } from 'components/CartList/styles'
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  ${CartWrapperStyles} {
+    width: 56rem;
+  }
+`
