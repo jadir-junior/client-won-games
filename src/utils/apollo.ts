30e591c150198e88228346fd1fd82bba25355d30
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-03 09:39:46 -0300

    criando a paginacao com fecthMore

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -5,6 +5,7 @@ import {
   NormalizedCacheObject
 } from '@apollo/client'
 
+import { concatPagination } from '@apollo/client/utilities'
 import { useMemo } from 'react'
 
 let apolloClient: ApolloClient<NormalizedCacheObject>
@@ -13,7 +14,15 @@ function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
-    cache: new InMemoryCache()
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            games: concatPagination()
+          }
+        }
+      }
+    })
   })
 }
 
