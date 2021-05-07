937901480173f7dead75fae8083cb9aa570fb724
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-07 18:46:25 -0300

    adicionando um overlay no dropdown para fechar ao clicar

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -28,4 +28,22 @@ describe('<Dropdown />', () => {
     expect(content).toHaveStyle({ opacity: 1 })
     expect(content?.getAttribute('aria-hidden')).toBe('false')
   })
+
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/i).parentElement
+    const overlay = content?.nextElementSibling
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content?.getAttribute('aria-hidden')).toBe('true')
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/i))
+
+    expect(overlay).toHaveStyle({ opacity: 1 })
+    expect(overlay!.getAttribute('aria-hidden')).toBe('false')
+
+    userEvent.click(overlay!)
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content?.getAttribute('aria-hidden')).toBe('true')
+  })
 })
