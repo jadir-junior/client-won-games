aafb15e0b2e82b9c78a914f5fbde815cfbde0596
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-12 09:01:22 -0300

    editando cartDropdown para adaptar ao Context

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -3,17 +3,11 @@ import * as S from './styles'
 import CartIcon from 'components/CartIcon'
 import CartList from 'components/CartList'
 import Dropdown from 'components/Dropdown'
-import { GameItemProps } from 'components/GameItem'
 
-export type CartDropdownProps = {
-  items?: GameItemProps[]
-  total?: string
-}
-
-const CartDropdown = ({ items, total }: CartDropdownProps) => (
+const CartDropdown = () => (
   <S.Wrapper>
     <Dropdown title={<CartIcon />}>
-      <CartList items={items} total={total} hasButton />
+      <CartList hasButton />
     </Dropdown>
   </S.Wrapper>
 )
