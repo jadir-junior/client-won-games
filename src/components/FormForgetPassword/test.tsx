e908bb61dec52f6f5c0a89a4058a146c32092dde
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-16 17:51:51 -0300

    ajustando os testes

--- src/components/FormForgetPassword/test.tsx
+++ src/components/FormForgetPassword/test.tsx
@@ -1,10 +1,19 @@
-import { screen } from '@testing-library/react'
-import { renderWithTheme } from 'utils/tests/helpers'
-
 import FormForgetPassword from '.'
+import { render } from 'utils/test-utils'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
 
 describe('<FormForgetPassword />', () => {
   it('should render the FormForgetPassword', () => {
-    renderWithTheme(<FormForgetPassword />)
+    render(<FormForgetPassword />)
   })
 })
