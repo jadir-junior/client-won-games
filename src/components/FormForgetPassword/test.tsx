46145ca446074b759eaa02104401cd2a44a8b330
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-17 11:29:01 -0300

    adicionando teste para o forgotpassword

--- src/components/FormForgetPassword/test.tsx
+++ src/components/FormForgetPassword/test.tsx
@@ -1,19 +1,61 @@
+import 'server.mock'
+
+import { render, screen } from 'utils/test-utils'
+
 import FormForgetPassword from '.'
-import { render } from 'utils/test-utils'
+import userEvent from '@testing-library/user-event'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
-const push = jest.fn()
+const query = {}
 
 useRouter.mockImplementation(() => ({
-  push,
-  query: '',
-  asPath: '',
-  route: '/'
+  query
 }))
 
 describe('<FormForgetPassword />', () => {
-  it('should render the FormForgetPassword', () => {
+  let inputEmail: HTMLElement
+  let buttonSendEmail: HTMLElement
+
+  beforeEach(() => {
     render(<FormForgetPassword />)
+
+    inputEmail = screen.getByPlaceholderText(/email/i)
+    buttonSendEmail = screen.getByRole('button', { name: /send email/i })
+  })
+
+  it('should render the FormForgetPassword', () => {
+    expect(inputEmail).toBeInTheDocument()
+    expect(buttonSendEmail).toBeInTheDocument()
+  })
+
+  it('should validate the email', async () => {
+    await userEvent.type(inputEmail, 'valid@email.com')
+
+    userEvent.click(buttonSendEmail)
+
+    expect(
+      await screen.findByText(/You just received an email!/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should show an invalid email', async () => {
+    await userEvent.type(inputEmail, 'invalidemail')
+
+    userEvent.click(buttonSendEmail)
+
+    expect(
+      await screen.findByText(/"email" must be a valid email/i)
+    ).toBeInTheDocument()
+  })
+
+  it('should show an inexistent email error', async () => {
+    await userEvent.type(inputEmail, 'false@email.com')
+
+    userEvent.click(buttonSendEmail)
+
+    expect(
+      await screen.findByText(/This email does not exist/i)
+    ).toBeInTheDocument()
   })
 })
