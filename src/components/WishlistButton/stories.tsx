a3db41def722e2738e178630649607cb2825defe
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-18 19:55:41 -0300

    session cacheada na chamada do /profile/me

--- src/components/WishlistButton/stories.tsx
+++ src/components/WishlistButton/stories.tsx
@@ -1,9 +1,11 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import WishlistButton from '.'
+import { Meta, Story } from '@storybook/react/types-6-0'
+import WishlistButton, { WishlistButtonProps } from '.'
 
 export default {
   title: 'WishlistButton',
   component: WishlistButton
 } as Meta
 
-export const Default: Story = () => <WishlistButton />
+export const Default: Story<WishlistButtonProps> = (args) => (
+  <WishlistButton {...args} />
+)
--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,9 +1,12 @@
 import ProfileForm, { FormProfileProps } from 'components/ProfileForm'
+import {
+  QueryProfileMe,
+  QueryProfileMeVariables
+} from 'graphql/generated/QueryProfileMe'
 
 import { GetServerSidePropsContext } from 'next'
 import Profile from 'templates/Profile'
 import { QUERY_PROFILE_ME } from 'graphql/queries/profile'
-import { QueryProfileMe } from 'graphql/generated/QueryProfileMe'
 import { initializeApollo } from 'utils/apollo'
 import protectedRoutes from 'utils/protected-routes'
 
@@ -19,11 +22,17 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session)
 
-  const { data } = await apolloClient.query<QueryProfileMe>({
-    query: QUERY_PROFILE_ME
+  const { data } = await apolloClient.query<
+    QueryProfileMe,
+    QueryProfileMeVariables
+  >({
+    query: QUERY_PROFILE_ME,
+    variables: {
+      identifier: session?.id as string
+    }
   })
 
   return {
-    props: { session, username: data.me?.username, email: data.me?.email }
+    props: { session, username: data?.user?.username, email: data?.user?.email }
   }
 }
