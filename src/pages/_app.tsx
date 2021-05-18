77baee85c6c216412e6a6161b2b8c49e7c88f5f4
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-18 11:17:56 -0300

    criando estrutura inicial do useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import GlobalStyles from 'styles/global'
 import Head from 'next/head'
 import NextNprogress from 'nextjs-progressbar'
 import { ThemeProvider } from 'styled-components'
+import { WishlistProvider } from 'hooks/use-wishlist'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
 
@@ -17,24 +18,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={3}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={3}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
