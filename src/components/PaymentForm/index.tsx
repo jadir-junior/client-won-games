fe8aa8174c082aa4c661e48e663a93173198143b
Jadir Junior <jadirjsjunior@gmail.com>
2021-05-20 13:47:00 -0300

    estilizando o CardElement

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -23,7 +23,10 @@ const PaymentForm = () => {
         </Heading>
 
         <CardElement
-          options={{ hidePostalCode: true }}
+          options={{
+            hidePostalCode: true,
+            style: { base: { fontSize: '16px' } }
+          }}
           onChange={handleChange}
         />
         {error && (
