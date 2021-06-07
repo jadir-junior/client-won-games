95c78ceff9f6b22fcff814f909488c1d887eec8b
Jadir Junior <jadirjsjunior@gmail.com>
2021-06-07 09:40:41 -0300

    clicando e esperando uma animação com click e wait

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -8,6 +8,18 @@ describe('Home Page', () => {
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', { name: /seven: the days long gone demo/i })
       cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(2) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /beneath a steel sky/i })
+      cy.findByRole('link', { name: /buy now/i })
+
+      cy.get('.slick-dots > :nth-child(3) > button').click()
+      cy.wait(500)
+
+      cy.findByRole('heading', { name: /craft the world - abandoned mines/i })
+      cy.findByRole('link', { name: /buy now/i })
     })
   })
 })
