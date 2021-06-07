2ab4fcd5da6896886a726aecf54f7f656b18fefe
Jadir Junior <jadirjsjunior@gmail.com>
2021-06-07 09:54:54 -0300

    refatorando: criando comandos customizados

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -2,24 +2,8 @@
 
 describe('Home Page', () => {
   it('should render home sections', () => {
-    // visitar a página
     cy.visit('/')
 
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', { name: /seven: the days long gone demo/i })
-      cy.findByRole('link', { name: /buy now/i })
-
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /beneath a steel sky/i })
-      cy.findByRole('link', { name: /buy now/i })
-
-      cy.get('.slick-dots > :nth-child(3) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /craft the world - abandoned mines/i })
-      cy.findByRole('link', { name: /buy now/i })
-    })
+    cy.shouldRenderBanner()
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -28,3 +28,22 @@
 import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('google', () => cy.visit('https://google.com'))
+
+Cypress.Commands.add('shouldRenderBanner', () => {
+  cy.get('.slick-slider').within(() => {
+    cy.findByRole('heading', { name: /seven: the days long gone demo/i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(2) > button').click()
+    cy.wait(500)
+
+    cy.findByRole('heading', { name: /beneath a steel sky/i })
+    cy.findByRole('link', { name: /buy now/i })
+
+    cy.get('.slick-dots > :nth-child(3) > button').click()
+    cy.wait(500)
+
+    cy.findByRole('heading', { name: /craft the world - abandoned mines/i })
+    cy.findByRole('link', { name: /buy now/i })
+  })
+})
