7d0db5e1150bab20ae91c2aad2a3008274adf2c8
Jadir Junior <jadirjsjunior@gmail.com>
2021-06-09 14:18:28 -0300

    ordenando jogos pelo preço

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -29,4 +29,26 @@ describe('Explore', () => {
     cy.findByRole('button', { name: /show more/i }).click()
     cy.getByDataCy('game-card').should('have.length', 30)
   })
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText(/free/i).should('exist')
+      })
+
+    cy.findByText(/highest to lowest/i).click()
+
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+
+    cy.getByDataCy('game-card')
+      .first()
+      .within(() => {
+        cy.findByText(/free/i).should('not.exist')
+      })
+  })
 })
